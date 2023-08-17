5634adda3650f86ac776cc17466217b50bf98f20
Peko-Glitch <mrgetpyked7@gmail.com>
2023-08-17 10:25:29 -0700

    all deleted start over

--- 0x03-debugging/3-convert_day.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-
-int convert_day(int month, int day)
-{
-    int days_in_month[] = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
-
-    return days_in_month[month - 1] + day;
-}
