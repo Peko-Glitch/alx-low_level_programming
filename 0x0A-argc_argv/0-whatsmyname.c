909a5f3d18e1253fd832b5ee7e94d54a9af69157
Peko-Glitch <mrgetpyked7@gmail.com>
2023-09-02 17:31:30 -0700

    fix0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: Always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
