5d90744f296470f1b69bcdd31272aa341b2181c2
Peko-Glitch <mrgetpyked7@gmail.com>
2023-09-02 19:19:05 -0700

    fix0

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,29 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
+ *
+ * Return: 0 for success, 1 for error
+ */
 
-int main(int argc, char *argv[]) {
-    int sum = 0;
+int main(int argc, char *argv[])
+{
+	int sum = 0;
 
-    if (argc == 1) {
-        printf("0\n");
-        return 0;
-    }
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-    for (int i = 1; i < argc; i++) {
-        for (int j = 0; argv[i][j] != '\0'; j++) {
-            if (!isdigit(argv[i][j])) {
-                printf("Error\n");
-                return 1;
-            }
-        }
+	for (int i = 1; i < argc; i++)
+	{
+		for (int j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 
-        int num = atoi(argv[i]);
-        sum += num;
-    }
+		int num = atoi(argv[i]);
 
-    printf("%d\n", sum);
+		sum += num;
+	}
 
-    return 0;
+	printf("%d\n", sum);
+
+	return (0);
 }
 
