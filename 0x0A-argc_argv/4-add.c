1459f826f53d88d9a7e521bc802da5c3a1513a3a
Peko-Glitch <mrgetpyked7@gmail.com>
2023-09-02 19:14:31 -0700

    Task4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[]) {
+    int sum = 0;
+
+    if (argc == 1) {
+        printf("0\n");
+        return 0;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        for (int j = 0; argv[i][j] != '\0'; j++) {
+            if (!isdigit(argv[i][j])) {
+                printf("Error\n");
+                return 1;
+            }
+        }
+
+        int num = atoi(argv[i]);
+        sum += num;
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
+}
+
