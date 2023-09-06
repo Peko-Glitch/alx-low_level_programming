c79a25b9f705c03c372058289107c11d27d7fe4e
Peko-Glitch <mrgetpyked7@gmail.com>
2023-09-06 14:55:36 -0700

    fix5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a message to the standard output.
+ * It demonstrates a simple program structure in C.
+ *
+ * Return: Always 0 (success)
+ */
 int main() {
-	puts("Programming is like building a multilingual puzzle/n");
+	puts("Programming is like building a multilingual puzzle\n");
 
 	return 0;
 }
