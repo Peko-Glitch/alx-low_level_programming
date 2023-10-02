fdc40e8a3f72d2c693c245a7bb78708c9d045718
Peko-Glitch <masterpekoov@gmail.com>
2023-10-02 09:32:51 -0700

    task0

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - Checks if the charater is lowercase.
+ * @c: The character to be checked
+ * Return: return te function.
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
